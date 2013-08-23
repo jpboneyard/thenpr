001e37f2dba5e109338b9e42e246a81724d26f5b
pkarl <pete.karl@gmail.com>
2013-08-23 07:18:34 -0400

    masonry now lays out posters in 5 columns

--- _js/script.js
+++ _js/script.js
@@ -1,38 +1,36 @@
+/*global $:false */
+
 $(document).ready(function(){
 
 	var $container = $('#poster-grid');
-	$container.imagesLoaded(function(){
-	  $container.masonry({
-	    itemSelector : '.photo-grid-item',
-	    isFitWidth:true
-	  });  
-	});	
-// masonry for poster grid
-	
-var images = ['hdr-sign.png', 'hdr-pennant.png', 'hdr-map.png'];	
-$('<img src="_img/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner');
-var images = ['hdr-bg.png'];	
-$('#banner-container').css({'background-image': 'url(_img/' + images[Math.floor(Math.random() * images.length)] + ')'});
-// filament carousel
 
+	// masonry for poster grid
+	$container.imagesLoaded(function(){
+
+		var msnry = new Masonry( document.querySelector('#poster-grid'), {
+			containerStyle: null,
+			itemSelector: '.photo-grid-item'
+		});
+
+	});	
+	
+	var images = ['hdr-sign.png', 'hdr-pennant.png', 'hdr-map.png'];	
+	$('<img src="_img/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner');
+
+	// var images = ['hdr-bg.png'];	
+	$('#banner-container').css({'background-image': 'url(_img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+
+	// filament carousel
 	$(".sponsor-trigger").click(function(){
 		var $this = $(this);
-	$(".sponsor-list").slideDown("slow");
+		$(".sponsor-list").slideDown("slow");
 		if($('.sponsor-list').is(":hidden")){
-            $this.html("But wait, there's more!");
-         }
-         else{
-            $this.html("That's all of em!");
-         }
+			$this.html("But wait, there's more!");
+		} else {
+			$this.html("That's all of em!");
+		}
 	}); // reveals more sponsor names
 
-	$(function(){ 
-		$(".sponsor-main-item img[title]").tooltip();
-	});
+	$(".sponsor-main-item img[title]").tooltip();
 
 });
-
-
-
-
-
