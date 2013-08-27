6cd9767c450365c05c6187e4ec4a0700d7e74710
JPB <jpboneyard@gmail.com>
2013-08-26 22:44:19 -0400

    type tweaks, new show section layout.

--- _includes/tour-slides.php
+++ _includes/tour-slides.php
@@ -1,114 +1,92 @@
 			<div class="wrapper shows-mod">
 				<div class="image-container">
+					<div class="gallery-show-cont">
+						<a class="gallery-show-wrap" href="2k13-philly.php">
+							<div class="gallery-info-cont">
+								<div class="gallery-info">
+									<p><span class="city-name">Philadelphia, PA</span></br> 9/14/13</br>Masthead Print Studio</br></p>
+									<div class="gallery-more-info">View more info</div>
+								</div> <!-- /gallery-info -->	
+							</div> <!-- /gallery-info-cont -->					
+							<img class="gallery-show-img" src="_img/gallery-show-philly-2k13.jpg"/>
+						</a><!-- /.gallery-show-wrap -->
+					</div><!-- /gallery-show-cont -->	
+				</div> <!-- /.image-container -->			
 
-		<div class="gallery-show-cont">
-					<a class="gallery-show-wrap" href="2k13-philly.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Philadelphia, PA</span></br> 9/14/13</br>Masthead Print Studio</br></p>
-								<div class="gallery-more-info">View more info</div>
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-philly-2k13.jpg"/>
-					</a><!-- /.gallery-show-wrap -->
-				</div><!-- /gallery-show-cont -->	
-
-				<div class="post-item-single">
-				<div class="gallery-show-cont media-block">
+			<section class="show-grid-list">
+				<div class="show-grid-mod media-block">
 					<a class="gallery-show-wrap" href="2k13-boston-pop-up.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Boston, MA</span></br> 6/1/13</br> Aviary Gallery</br></p>
-								<div class="gallery-more-info">View more info</div>
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-boston-pop-up-2k13.jpg"/>
-					</a><!-- /.gallery-show-wrap -->
-				</div><!-- /gallery-show-cont -->							
+						<div class="show-item">
+							<img class="show-img-sm" src="_img/tz-npr-jamaica-plain.jpg" />
+							<div class="show-caption">Jamaica Plain</div>
+						</div><!-- /show-item -->	
+					</a>	
+				</div> <!-- /show-grid-mod -->	
 
-				<div class="gallery-show-cont media-block">
-					<a class="gallery-show-wrap" href="2k13-burlington.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Burlington, VT</span></br> 4/3/13</br> Select Design</br></p>
-								<div class="gallery-more-info">View more info</div>
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-burlington-2k13.jpg"/>
-					</a><!-- /.gallery-show-wrap -->
-				</div><!-- /gallery-show-cont -->	
+				<div class="show-grid-mod media-block">
+					<div class="show-item">
+						<a class="gallery-show-wrap" href="2k13-boston.php">
+							<img class="show-img-sm" src="_img/tz-npr-boston.jpg" />
+							<div class="show-caption">Boston</div>
+						</a>	
+					</div> <!-- /show-item -->	
+				</div> <!-- /show-grid-mod -->	
 
-				<div class="gallery-show-cont media-block">
-					<a class="gallery-show-wrap" href="2k13-rochester.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Rochester, NY</span></br> 4/6/13</br> 1975 Gallery</br></p>
-								<div class="gallery-more-info">View more info</div>
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->	
-						<img class="gallery-show-img" src="_img/gallery-show-rochester-2k13.jpg"/>
-					</a> <!-- /gallery-show-wrap -->
-				</div> <!-- /gallery-show-cont -->
-				 
-				<div class="gallery-show-cont media-block">
-					<a class="gallery-show-wrap" href="2k13-detroit.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Detroit, MI</span></br> 4/10/13</br> 5 E Gallery</br></p>
-								<div class="gallery-more-info">View more info</div>							
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-detroit-2k13.jpg"/>
-					 </a><!-- /gallery-show-wrap -->	
-				</div> <!-- /gallery-show-cont -->	
+				<div class="show-grid-mod media-block">
+					<div class="show-item">
+						<a class="gallery-show-wrap" href="2k13-richmond.php">
+							<img class="show-img-sm" src="_img/tz-npr-richmond.jpg" />
+							<div class="show-caption">Richmond</div>
+						</a>	
+					</div> <!-- /show-item -->	
+				</div> <!-- /show-grid-mod -->	
+			
+				<div class="show-grid-mod media-block">
+					<div class="show-item">
+						<a class="gallery-show-wrap" href="2k13-st-louis.php">
+							<img class="show-img-sm" src="_img/tz-npr-st-louis.jpg" />
+							<div class="show-caption">St. Louis</div>
+						</a>	
+					</div> <!-- /show-item -->	
+				</div> <!-- /show-grid-mod -->	
 
-				<div class="gallery-show-cont media-block">
-					<a class="gallery-show-wrap" href="2k13-minneapolis.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Minneapolis, MN</span></br> 4/13/13</br> Co Exhibtions</br></p>
-								<div class="gallery-more-info">View more info</div>						
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-minneapolis-2k13.jpg"/>
-					</a><!-- /gallery-show-wrap -->
-				</div> <!-- /gallery-show-cont -->
+				<div class="show-grid-mod media-block">
+					<div class="show-item">
+						<a class="gallery-show-wrap" href="2k13-minneapolis.php">
+							<img class="show-img-sm" src="_img/tz-npr-minneapolis.jpg" />
+							<div class="show-caption">Minneapolis</div>
+						</a>	
+					</div> <!-- /show-item -->	
+				</div> <!-- /show-grid-mod -->	
 
-				<div class="gallery-show-cont media-block">
-					<a class="gallery-show-wrap" href="2k13-st-louis.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">St. Louis</span></br> 4/16/13</br> Mad Art Gallery</br></p>
-								<div class="gallery-more-info">View more info</div>
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-st-louis-2k13.jpg"/>
-					</a><!-- /gallery-show-wrap -->	
-				</div> <!-- /gallery-show-cont -->
+				<div class="show-grid-mod media-block">
+					<div class="show-item">
+						<a class="gallery-show-wrap" href="2k13-detroit.php">
+							<img class="show-img-sm" src="_img/tz-npr-detroit.jpg" />
+							<div class="show-caption">Detroit</div>
+						</a>	
+					</div> <!-- /show-item -->	
+				</div> <!-- /show-grid-mod -->	
 
-				<div class="gallery-show-cont media-block">
-					<a class="gallery-show-wrap" href="2k13-richmond.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Richmond, VA</span></br> 4/20/13</br> Gallery 5</br></p>
-							<div class="gallery-more-info">View more info</div>
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-richmond-2k13.jpg"/>
-					</a><!-- /gallery-show-wrap -->
-				</div> <!-- /gallery-show-cont -->	
+				<div class="show-grid-mod media-block">
+					<div class="show-item">
+						<a class="gallery-show-wrap" href="2k13-rochester.php">
+							<img class="show-img-sm" src="_img/tz-npr-rochester.jpg" />
+							<div class="show-caption">Rochester</div>
+						</a>	
+					</div> <!-- /show-item -->	
+				</div> <!-- /show-grid-mod -->		
 
-				<div class="gallery-show-cont media-block">
-					<a class="gallery-show-wrap" href="2k13-boston.php">
-						<div class="gallery-info-cont">
-							<div class="gallery-info">
-								<p><span class="city-name">Boston, MA</span></br> 5/3/13</br> Upstatement</br></p>
-							<div class="gallery-more-info">View more info</div>
-							</div> <!-- /gallery-info -->	
-						</div> <!-- /gallery-info-cont -->					
-						<img class="gallery-show-img" src="_img/gallery-show-boston-2k13.jpg"/>
-					</a><!-- /gallery-show-wrap -->
-				</div> <!-- /gallery-show-cont -->		
+				<div class="show-grid-mod media-block">
+					<div class="show-item">
+						<a class="gallery-show-wrap" href="2k13-burlington.php">
+							<img class="show-img-sm" src="_img/tz-npr-burlington.jpg" />
+							<div class="show-caption">Burlington</div>
+						</a>	
+					</div> <!-- /show-item -->	
+				</div> <!-- /show-grid-mod -->								
+		</section> <!-- /show-grid-list -->	
+	</div> <!-- /.wrapper -->
 
-				</div> <!-- /.image-container -->
-			</div> <!-- /.wrapper -->
+
+		
